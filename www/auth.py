d93ee060030c3c25c122cf98e944ab0a4a26232f
Joshua Molyneux <joshuamolyneux97@hotmail.co.uk>
2022-01-24 21:13:04 +0000

    Update auth.py

--- www/auth.py
+++ www/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 from datetime import datetime
 
 auth = Blueprint('auth', __name__)
@@ -11,10 +11,21 @@ def login():
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-    # if request.method == 'POST':
-    #     email = request.form.get('email')
-    #     fullName = request.form.get('fullName')
-    #     password = request.form.get('password')
-    #     passwordConfirm = request.form.get('passwordConfirm')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        fullName = request.form.get('fullName')
+        password = request.form.get('password')
+        passwordConfirm = request.form.get('passwordConfirm')
 
-        return render_template("register.html",  datetime=str(datetime.now().year))
+        if len(email) < 4:
+            flash('Email must be greater 3 characters', category='error')
+        elif len(fullName) < 4:
+            flash('Full Name must be greater than 3 characters', category='error')
+        elif password != passwordConfirm:
+            flash('Passwords do not match', category='error')
+        elif len(password) < 8:
+            flash('Password must be greater than 7 characters', category='error')
+        else:
+            flash('Account successfully created!', category='success')
+
+    return render_template("register.html",  datetime=str(datetime.now().year))
