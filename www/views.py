44bd641a64cd5a87bd9fa5ec37b073e941750bf5
Joshua Molyneux <joshuamolyneux97@hotmail.co.uk>
2022-01-24 17:25:54 +0000

    Preliminary cross over from other website

---
+++ www/views.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template
+from datetime import datetime
+import subprocess as sp
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/')
+def root():
+    return render_template("home.html", datetime=str(datetime.now().year))
+
+
+# @views.route('/home')
+# def home():
+#     return render_template("home.html", datetime=str(datetime.now().year))
+#
+#
+# @views.route('/index')
+# def index():
+#     return render_template("home.html", datetime=str(datetime.now().year))
+#
+#
+# @views.route('/projects')
+# def projects():
+#   return render_template("projects.html", datetime=str(datetime.now().year))
+#
+#
+# @views.route('/about')
+# def about():
+#     return render_template("about.html", datetime=str(datetime.now().year))
+#
+#
+# @views.route('/contact')
+# def contact():
+#     return render_template("contact.html", datetime=str(datetime.now().year))
