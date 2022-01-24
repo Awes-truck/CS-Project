e3cc0b65a2c7cfed08372cac6adf26af20887c59
Joshua Molyneux <joshuamolyneux97@hotmail.co.uk>
2022-01-24 17:26:40 +0000

    Update __init__.py

--- www/__init__.py
+++ www/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'b997BDubiBD18NDFnjnler'
+    app.config['SECRET_KEY'] = 'sgeswgw43twsfwq3fafsdfq3'
 
     from.views import views
 
