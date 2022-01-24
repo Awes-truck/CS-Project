335a3dceec0951a13562b7e3e758de5149dcc16c
Joshua Molyneux <joshuamolyneux97@hotmail.co.uk>
2022-01-24 19:51:21 +0000

    Update views.py

--- www/views.py
+++ www/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 from datetime import datetime
 
 views = Blueprint('views', __name__)
@@ -14,6 +14,15 @@ def home():
     return render_template("news.html", datetime=str(datetime.now().year))
 
 
+@views.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 # @views.route('/index')
 # def index():
 #     return render_template("home.html", datetime=str(datetime.now().year))
