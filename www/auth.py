66014b8bf703ebfd215e4bed32f83f13a8edd8af
Joshua Molyneux <joshuamolyneux97@hotmail.co.uk>
2022-01-24 20:35:08 +0000

    Update auth.py

--- www/auth.py
+++ www/auth.py
@@ -1,13 +1,20 @@
 from flask import Blueprint, render_template, request
+from datetime import datetime
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login', methods=['GET', 'POST'])
+@auth.route('/login',  datetime=str(datetime.now().year)methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    return render_template("login.html",  datetime=str(datetime.now().year))
 
 
 @auth.route('/register', methods=['GET', 'POST'])
 def signup():
-    return render_template("register.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        fullName = request.form.get('fullName')
+        password = request.form.get('password')
+        passwordConfirm = request.form.get('passwordConfirm')
+
+        return render_template("register.html",  datetime=str(datetime.now().year))
