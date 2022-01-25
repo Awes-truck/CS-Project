428917ded6313facfbbb2ffbb43971a91a927bf8
Awestruck <joshuamolyneux97@hotmail.co.uk>
2022-01-25 14:51:30 +0000

    Fixed autocomplete not showing county and postal town

--- www/static/js/auto-complete.js
+++ www/static/js/auto-complete.js
@@ -2,8 +2,8 @@ var placeSearch, autocomplete;
       var componentForm = {
         street_number: 'short_name',
         route: 'long_name',
-        locality: 'long_name',
-        administrative_area_level_1: 'short_name',
+        postal_town: 'long_name',
+        administrative_area_level_2: 'short_name',
         postal_code: 'short_name'
       };
 
