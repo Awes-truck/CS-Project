a5bb5b3d56ba4ef92eb104137b0e91ca75e17b66
Awestruck <joshuamolyneux97@hotmail.co.uk>
2022-01-25 15:33:29 +0000

    Adding variables to the view

--- www/auth.py
+++ www/auth.py
@@ -13,15 +13,28 @@ def login():
 def register():
     if request.method == 'POST':
         email = request.form.get('email')
-        fullName = request.form.get('fullName')
+        full_name = request.form.get('fullName')
         password = request.form.get('password')
-        passwordConfirm = request.form.get('passwordConfirm')
+        password_confirm = request.form.get('passwordConfirm')
+        postal_list = [
+            'street_number',
+            'route',
+            'postal_town',
+            'administrative_area_level_2',
+            'postal_code'
+        ]
+        full_address_list = []
+        separator = ', '
+        for i in postal_list:
+            full_address_list.append(request.form.get(i))
+
+        full_address = separator.join(full_address_list)
 
         if len(email) < 4:
             flash('Email must be greater 3 characters', category='error')
-        elif len(fullName) < 4:
+        elif len(full_name) < 4:
             flash('Full Name must be greater than 3 characters', category='error')
-        elif password != passwordConfirm:
+        elif password != password_confirm:
             flash('Passwords do not match', category='error')
         elif len(password) < 8:
             flash('Password must be greater than 7 characters', category='error')
