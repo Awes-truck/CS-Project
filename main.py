4aa240147cc5e44334cc7f8a646c56e9e3c791e7
Awestruck <joshuamolyneux97@hotmail.co.uk>
2022-01-27 10:01:06 +0000

    Database integration

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
 from www import create_app
-from flask_mysqldb import MySQL
+#import mysql.connector
+import pymysql
 
 app = create_app()
 
 app.config['SECRET_KEY'] = 'sgeswgw43twsfwq3fafsdfq3'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'wgccc'
+host = 'localhost'
+port = 3306
+user = 'root'
+password = ''
 
-mysql = MySQL(app)
+connect = pymysql.connect(
+    host=host,
+    port=port,
+    user=user,
+    password=password,
+)
+
+
+def setup_database():
+    cursor = connect.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS wgccc")
+    connect.commit()
+    connect.close()
+
+    new_db = pymysql.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database='wgccc'
+    )
+    cursor = new_db.cursor()
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS users
+        (id INT(6) PRIMARY KEY AUTO_INCREMENT,
+        first_name VARCHAR(255),
+        family_name VARCHAR(255),
+        email VARCHAR(255))''')
+    new_db.commit()
+    new_db.close()
+
+
+setup_database()
 
 if __name__ == '__main__':
     app.run(debug=True)
