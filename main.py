00f7597a6665576b8c97b4961ddcdc7b23e982b1
Awestruck <joshuamolyneux97@hotmail.co.uk>
2022-01-27 12:28:28 +0000

    Revert "Update main.py"
    This reverts commit 50104da315f3272d8faf9920ffa27e2540ee963c.

--- main.py
+++ main.py
@@ -10,6 +10,39 @@ port = 3306
 user = 'root'
 password = ''
 
+connect = pymysql.connect(
+    host=host,
+    port=port,
+    user=user,
+    password=password,
+)
+
+
+def setup_database():
+    cursor = connect.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS wgccc")
+    connect.commit()
+    connect.close()
+
+    new_db = pymysql.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database='wgccc'
+    )
+    cursor = new_db.cursor()
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS users
+        (id INT(6) PRIMARY KEY AUTO_INCREMENT,
+        first_name VARCHAR(255),
+        family_name VARCHAR(255),
+        email VARCHAR(255))''')
+    new_db.commit()
+    new_db.close()
+
+
+setup_database()
 
 if __name__ == '__main__':
     app.run(debug=True)
