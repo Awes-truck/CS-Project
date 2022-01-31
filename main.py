aec70272fde76662dd0d83bd1afd8982063d562d
Awestruck <joshuamolyneux97@hotmail.co.uk>
2022-01-31 12:27:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,48 +11,44 @@ LOCAL_SQL_MODE = False
 
 @app.before_first_request
 def before_first_request_func():
+    global LOCAL_SQL_MODE
     o = urlparse(request.base_url)
-    print(o.hostname)
+    hostname = o.hostname
+    if hostname == "localhost":
+        sql_host = 'localhost'
+        sql_port = 3306
+        sql_user = 'root'
+        sql_password = ''
 
+        connect = pymysql.connect(
+            host=sql_host,
+            port=sql_port,
+            user=sql_user,
+            password=sql_password,
+        )
 
-def setup_database():
-    cursor = connect.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS wgccc")
-    connect.commit()
-    connect.close()
+        cursor = connect.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS wgccc")
+        connect.commit()
+        connect.close()
 
-    new_db = pymysql.connect(
-        host=host,
-        port=port,
-        user=user,
-        password=password,
-        database='wgccc'
-    )
-    cursor = new_db.cursor()
-    cursor.execute(
-        '''CREATE TABLE IF NOT EXISTS users
-        (id INT(6) PRIMARY KEY AUTO_INCREMENT,
-        first_name VARCHAR(255),
-        family_name VARCHAR(255),
-        email VARCHAR(255))''')
-    new_db.commit()
-    new_db.close()
+        new_db = pymysql.connect(
+            host=sql_host,
+            port=sql_port,
+            user=sql_user,
+            password=sql_password,
+            database='wgccc'
+        )
 
-
-# if not LOCAL_SQL_MODE:
-#     host = 'localhost'
-#     port = 3306
-#     user = 'root'
-#     password = ''
-#
-#     connect = pymysql.connect(
-#         host=host,
-#         port=port,
-#         user=user,
-#         password=password,
-#     )
-#
-#     setup_database()
+        cursor = new_db.cursor()
+        cursor.execute(
+            '''CREATE TABLE IF NOT EXISTS users
+            (id INT(6) PRIMARY KEY AUTO_INCREMENT,
+            first_name VARCHAR(255),
+            family_name VARCHAR(255),
+            email VARCHAR(255))''')
+        new_db.commit()
+        new_db.close()
 
 
 if __name__ == '__main__':
