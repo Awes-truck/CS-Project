43c21d520f35826f8a122da1cf5fa301013897e4
Joshua Molyneux <joshuamolyneux97@hotmail.co.uk>
2022-01-31 19:44:21 +0000

    Don't need

--- www/models.py
+++
@@ -1,9 +0,0 @@
-from . import mysql
-from flask_login import UserMixin
-
-
-class User(mysql.Model, UserMixin):
-    id = mysql.Column(mysql.Integer, primary_key=True)
-    email = mysql.Column(mysql.String(150), unique=True)
-    password = mysql.Column(mysql.String(150))
-    full_name = mysql.Column(mysql.String(150))
