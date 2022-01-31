502edf99172cd2896a476d886f4f34e4f6554657
Joshua Molyneux <joshuamolyneux97@hotmail.co.uk>
2022-01-31 19:44:50 +0000

    Moved all of config and sql_connect() function to init.py

--- www/__init__.py
+++ www/__init__.py
@@ -1,8 +1,28 @@
 from flask import Flask
+import pymysql
+
+
+def sql_connect(host, port, user, password, database):
+    connect = pymysql.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database=database
+    )
+    return connect
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'sgeswgw43twsfwq3fafsdfq3'
+    # will be set dependent on where the app is hosted
+    app.config['SQL_HOST'] = ''
+    app.config['SQL_PORT'] = 0
+    app.config['SQL_USER'] = ''
+    app.config['SQL_PASSWORD'] = ''
+    app.config['SQL_DATABASE'] = ''
 
     from.views import views
     from.auth import auth
