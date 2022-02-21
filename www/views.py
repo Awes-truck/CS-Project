a01519d37aed99e0106f379ac4aa009053ba9ed8
Awestruck <joshuamolyneux97@hotmail.co.uk>
2022-02-21 12:12:19 +0000

    Created success page on payment success

--- www/views.py
+++ www/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, request, session, flash, url_for
 from datetime import datetime
 from urllib.parse import urlparse
+from .__init__ import sql_connect, login_required
 import stripe
 import os
 stripe.api_key = os.getenv('STRIPE_API_TEST_SK')
@@ -49,13 +50,27 @@ def subscriptions():
                 'quantity': 1
             }],
             mode='subscription',
-            success_url='http://' + hostname + port + '/subscriptions',
+            success_url='http://' + hostname + port
+            + '/success?session_id={CHECKOUT_SESSION_ID}',
             cancel_url='http://' + hostname + port + '/subscriptions'
         )
+        session['stripe_session'] = stripe_session.id
         return redirect(stripe_session.url, code=303)
     return render_template("subscriptions.html", datetime=str(datetime.now().year))
 
 
+@views.route('/success')
+@login_required
+def success():
+    if 'stripe_session' not in session or 'session_id' not in request.args or session['stripe_session'] != request.args.get('session_id'):
+        flash("You cannot access this page from here", category='error')
+        return redirect(url_for('views.home'))
+    stripe_session = stripe.checkout.Session.retrieve(
+        request.args.get('session_id')
+    )
+    session.pop('stripe_session', None)
+
+    return render_template("success.html", datetime=str(datetime.now().year))
 # @views.route('/index')
 # def index():
 #     return render_template("home.html", datetime=str(datetime.now().year))
