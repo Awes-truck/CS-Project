b504e02b9b71b30d0375c3bec7376b3ecb805cef
Awestruck <joshuamolyneux97@hotmail.co.uk>
2022-02-23 11:59:39 +0000

    Modal implementation

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -2,3 +2,27 @@ $(".navbar .nav-link").on("click", function(){
     $(".navbar").find(".active_id").removeClass("active_id");
     $(this).addClass("active_id");
 });
+
+var modals = document.getElementsByClassName('modal');
+var btns = document.getElementsByClassName("openmodal");
+var spans = document.getElementsByClassName("close");
+
+for(let i=0;i<btns.length;i++){
+    btns[i].onclick = function() {
+        modals[i].style.display = "block";
+    }
+}
+
+for(let i=0;i<spans.length;i++){
+    spans[i].onclick = function() {
+        modals[i].style.display = "none";
+    }
+ }
+
+ window.onclick = function(event) {
+     for(let i=0;i<modals.length;i++){
+         if (event.target == modals[i]) {
+             modals[i].style.display = "none";
+         }
+     }
+ }
