f68104da99cb86506460ea734dbf448ccdc45628
Awestruck <joshuamolyneux97@hotmail.co.uk>
2022-04-01 11:39:05 +0100

    Adding a country code to phone number

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -26,3 +26,19 @@ for(let i=0;i<spans.length;i++){
          }
      }
  }
+
+ var phone_field = document.getElementById('phone');
+ phone_field.style.position = 'absolute';
+ phone_field.style.top = '-9999px';
+ phone_field.style.left = '-9999px';
+ phone_field.parentElement.insertAdjacentHTML('beforeend', '<div><input type="tel" class="form-control" id="_phone"></div>');
+ var intl_phone_field = document.getElementById('_phone');
+ var intl_phone_iti = window.intlTelInput(intl_phone_field, {
+     initialCountry: 'gb' ,
+     separateDialCode: true,
+     utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/16.0.4/js/utils.js",
+ });
+ intl_phone_iti.setNumber(phone_field.value);
+ intl_phone_field.addEventListener('blur', function() {
+     phone_field.value = intl_phone_iti.getNumber();
+ });
