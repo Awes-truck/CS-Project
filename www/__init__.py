8c4cbd3dc559f61f210fc8f17c0445a1ba556866
Awestruck <joshuamolyneux97@hotmail.co.uk>
2022-04-04 12:16:06 +0100

    Init.py Comments

--- www/__init__.py
+++ www/__init__.py
@@ -7,7 +7,7 @@ import os
 project_folder = os.path.expanduser('www')  # adjust as appropriate
 load_dotenv(os.path.join(project_folder, '.env'))
 
-
+# A wrapper used to check if a user is logged in
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -18,7 +18,7 @@ def login_required(f):
             return redirect(url_for('auth.login'))
     return wrap
 
-
+# SQL Connection Function
 def sql_connect(host, port, user, password, database):
     connect = pymysql.connect(
         host=host,
@@ -29,13 +29,13 @@ def sql_connect(host, port, user, password, database):
     )
     return connect
 
-
+# Create the Flask Applcation
 def create_app():
     app = Flask(__name__)
-
+    # The app needs a secret key to work, grab it from the .env file
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-    # will be set dependent on where the app is hosted
 
+    # Create our page blueprints
     from.views import views
     from.auth import auth
 
